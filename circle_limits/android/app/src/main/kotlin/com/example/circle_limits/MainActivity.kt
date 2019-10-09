da5f08108c53d05a25119bcb33530010435fb2a6
Mikael Vejdemo-Johansson <michiexile@gmail.com>
2019-10-09 14:00:19 -0400

    Initial flutter project

---
+++ circle_limits/android/app/src/main/kotlin/com/example/circle_limits/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.circle_limits
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
